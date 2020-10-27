ca823494a8a2069d320ed482de6b994b4ba0a445
Jyri Lindroos <jyri.lindroos@keuda.fi>
2020-10-27 10:59:02 +0200

    Add project files.

---
+++ CSHARPharjoituksia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSHARPharjoituksia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool vastaus = onkoSamat(1, 2);
+            Console.WriteLine("Olivatko luvut samat: " + vastaus);
+        }
+        public static bool onkoSamat(int luku1, int luku2)
+        {
+            if(luku1 == luku2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
